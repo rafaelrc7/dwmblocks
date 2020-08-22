bc8745d53e400e9322e8061d9dacdaca5e99dc4a
rafaelrc7 <rafaelrc7@gmail.com>
2020-08-22 16:51:48 -0300

    Fixed gpu script for driver 450xx

--- blocks/gpu.sh
+++ blocks/gpu.sh
@@ -5,7 +5,7 @@ case $BUTTON in
 	3) st -c htop -e nvtop & ;;
 esac
 
-gpu_usage=$(nvidia-smi | awk 'NR==9{print substr($13, 1, length($13))}')
+gpu_usage=$(nvidia-smi | awk 'NR==10{print substr($13, 1, length($13))}')
 gpu_temp=$(nvidia-smi | grep -Po '[0-9][0-9]*(?=C)')
 
 printf "[GPU %3s %2d°C] " $gpu_usage $gpu_temp
